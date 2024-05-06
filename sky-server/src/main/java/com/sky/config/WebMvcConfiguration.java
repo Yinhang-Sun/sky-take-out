a4a0101089428c5fd46db0532a09b761d201d2d6
yinhang <sunyinhang@gmail.com>
2024-05-06 12:42:54 -0700

    Employee pagination query

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -1,10 +1,13 @@
 package com.sky.config;
 
 import com.sky.interceptor.JwtTokenAdminInterceptor;
+import com.sky.json.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
@@ -14,6 +17,8 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.List;
 
 /**
  * Configuration class, register web layer related components
@@ -66,5 +71,22 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
         log.info("Start setting up static resource mapping...");
         registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    /**
+     * Extend Message Converters of the Spring MVC
+     * @param converters
+     */
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        log.info("Extend Message Converters ...");
+        //Create a message converter object
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        //set up an object converter for the message converter,
+        // which can serialize Java objects into json data
+        converter.setObjectMapper(new JacksonObjectMapper());
+        //Add your own converter to the container
+        converters.add(0, converter);
+
     }
 }
