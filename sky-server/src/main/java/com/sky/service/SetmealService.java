8de3a5c735395a4c7bbc2e1f737866abe243922d
yinhang <sunyinhang@gmail.com>
2024-05-09 11:26:47 -0700

    Setmeal pagination query

--- sky-server/src/main/java/com/sky/service/SetmealService.java
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -1,6 +1,8 @@
 package com.sky.service;
 
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
 
 public interface SetmealService {
 
@@ -10,5 +12,12 @@ public interface SetmealService {
      * @return
      */
     void saveWithDish(SetmealDTO setmealDTO);
+
+    /**
+     * Setmeal pagination query
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO);
 
 }
