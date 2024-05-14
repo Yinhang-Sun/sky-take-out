6f6a8a603d47bbfe857fbe96c0bde5e6e25c5b08
yinhang <sunyinhang@gmail.com>
2024-05-14 13:11:22 -0700

    Cache setmeal with Spring Cache

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -10,6 +10,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -34,6 +35,7 @@ public class SetmealController {
      */
     @PostMapping
     @ApiOperation("Add new setmeal")
+    @CacheEvict(cacheNames = "setmealCache", key = "#setmealDTO.categoryId") //key: setmealCache::100
     public Result save(@RequestBody SetmealDTO setmealDTO) {
         log.info("Add new setmeal: {}", setmealDTO);
         setmealService.saveWithDish(setmealDTO);
@@ -60,6 +62,7 @@ public class SetmealController {
      */
     @DeleteMapping
     @ApiOperation("Delete setmeal in batch")
+    @CacheEvict(cacheNames = "setmealCache", allEntries = true)
     public Result delete(@RequestParam List<Long> ids) {
         log.info("Delete setmeal in batch: {}", ids);
         setmealService.deleteBatch(ids);
@@ -86,6 +89,7 @@ public class SetmealController {
      */
     @PutMapping
     @ApiOperation("Update setmeal")
+    @CacheEvict(cacheNames = "setmealCache", allEntries = true)
     public Result update(@RequestBody SetmealDTO setmealDTO) {
         log.info("Update setmeal: {}", setmealDTO);
         setmealService.update(setmealDTO);
@@ -98,6 +102,7 @@ public class SetmealController {
      */
     @PostMapping("/status/{status}")
     @ApiOperation("Start or stop setmeal")
+    @CacheEvict(cacheNames = "setmealCache", allEntries = true)
     public Result startOrStop(@PathVariable Integer status, Long id) {
         log.info("Start or stop setmeal: {}", id);
         setmealService.startOrStop(status, id);
